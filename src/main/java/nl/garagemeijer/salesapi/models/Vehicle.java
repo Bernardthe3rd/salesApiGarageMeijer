03a7297bb163b6c8c5751e4b04f8709ec5627e0d
Bernardthe3rd <bammeijer@outlook.com>
2024-09-25 15:59:28 +0200

    created first model vehicle

---
+++ src/main/java/nl/garagemeijer/salesapi/models/Vehicle.java
@@ -0,0 +1,122 @@
+package nl.garagemeijer.salesapi.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Vehicles")
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String vinNumber;
+    private String brand;
+    private String model;
+    private String type;
+    private int year;
+    private String licensePlate;
+    private int mileage;
+    private String color;
+    private String fuelType;
+    private Double engineCapacity;
+    private LocalDate firstRegistrationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVinNumber() {
+        return vinNumber;
+    }
+
+    public void setVinNumber(String vinNumber) {
+        this.vinNumber = vinNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public Double getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(Double engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    public LocalDate getFirstRegistrationDate() {
+        return firstRegistrationDate;
+    }
+
+    public void setFirstRegistrationDate(LocalDate firstRegistrationDate) {
+        this.firstRegistrationDate = firstRegistrationDate;
+    }
+}
