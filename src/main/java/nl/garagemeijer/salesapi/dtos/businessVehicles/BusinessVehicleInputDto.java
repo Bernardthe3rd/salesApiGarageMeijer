a63a91aad717e9e0e73713acac8482262a6f7c87
Bernardthe3rd <bammeijer@outlook.com>
2024-10-05 16:43:46 +0200

    set some constraints on certain field of the business vehicle

--- src/main/java/nl/garagemeijer/salesapi/dtos/businessVehicles/BusinessVehicleInputDto.java
+++ src/main/java/nl/garagemeijer/salesapi/dtos/businessVehicles/BusinessVehicleInputDto.java
@@ -1,5 +1,7 @@
 package nl.garagemeijer.salesapi.dtos.businessVehicles;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import nl.garagemeijer.salesapi.enums.BusinessUsageType;
@@ -21,8 +23,11 @@ public class BusinessVehicleInputDto {
     private String fuelType;
     private Double engineCapacity;
     private LocalDate firstRegistrationDate;
+    @NotEmpty(message = "please set true if the vehicle is company owned else set false")
     private Boolean companyOwned;
+    @NotNull(message = "please fill in the cargo capacity in liters")
     private Double cargoCapacity;
+    @NotNull(message = "please fill in the field of which business the vehicle is going to be used")
     private BusinessUsageType businessUsage;
 
 }
