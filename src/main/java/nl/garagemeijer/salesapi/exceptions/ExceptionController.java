0966c9e901fa2f02a4567846d52cce206b38c5f2
Bernardthe3rd <bammeijer@outlook.com>
2024-10-12 16:23:41 +0200

    created recordnotfoundexception and exception controller

---
+++ src/main/java/nl/garagemeijer/salesapi/exceptions/ExceptionController.java
@@ -0,0 +1,22 @@
+package nl.garagemeijer.salesapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneralException(Exception ex) {
+        String message = "Unexpected error: " + ex.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    public ResponseEntity<Object> handleRecordNotFoundException(RecordNotFoundException ex) {
+        String message = "Record not found: " + ex.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+}
