4eb69b1d04d498c0897b1ee8c4dfb6a69c2bcf83
Bernardthe3rd <bammeijer@outlook.com>
2024-10-30 11:45:37 +0100

    implemented global cors configuration

---
+++ src/main/java/nl/garagemeijer/salesapi/config/GlobalCorsConfiguration.java
@@ -0,0 +1,21 @@
+package nl.garagemeijer.salesapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
